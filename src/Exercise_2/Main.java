036d00bbc2af7444f6fae619b03c82c7d40479a8
Maciej Dluzen <maciej.dluzen@yahoo.com>
2019-11-24 20:09:56 +0000

    Revision C

--- src/Exercise_2/Main.java
+++ src/Exercise_2/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         //Obliczenia zlozonej ksztaltki
         Figure[] figures = new Figure[3];
         figures[0] = new Circle(3);
@@ -12,16 +11,18 @@ public class Main {
         figures[2] = new Square(3);
 
         ComplexFitting cf = new ComplexFitting();
-        System.out.println(cf.calculateComplexCuttingCost(figures));
-        System.out.println(cf.calculateComplexMaterialCost(figures));
-
+        double cuttingCost = cf.calculateComplexCuttingCost(figures);
+        double materialCost = cf.calculateComplexMaterialCost(figures);
+        System.out.println("Koszt wyciecia złozonej kształtki wynosi: " + cuttingCost);
+        System.out.println("Koszt materiału do wyciecia złozonej kształtki wynosi: " + materialCost);
 
         // Obliczenia prostej ksztaltki
         Figure rect = new Rectangle(2,3);
 
-        SimpleFitting bs = new SimpleFitting();
-        System.out.println(bs.calculateSimpleCuttingCost(rect));
-
-
+        SimpleFitting sf = new SimpleFitting();
+        double cuttingCost2 = sf.calculateSimpleCuttingCost(rect);
+        double materialCost2 = sf.calculateSimpleMaterialCost(rect);
+        System.out.println("Koszt wyciecia prostej kształtki wynosi: " + cuttingCost2);
+        System.out.println("Koszt materiału do wyciecia prostej kształtki wynosi: " + materialCost2);
     }
 }
